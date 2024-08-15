9183b5079c2fc632423ad07be9d5576f27c65c32
Shahrizan <shahrizan.code@gmail.com>
2024-08-15 23:19:22 +0700

    add ssl

--- utils/database.js
+++ utils/database.js
@@ -1,7 +1,11 @@
 import { Pool } from 'pg';
 
 // Creates a global connection pool
-const pool = new Pool();
+const pool = new Pool({
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 export const query = (text, params) => {
   return pool.query(text, params);
