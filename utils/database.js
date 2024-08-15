9d6426aad4e844f4a3b053e7bbdd206b5de6b89a
Shahrizan <shahrizan.code@gmail.com>
2024-08-15 22:37:50 +0700

    delete ssl setting

--- utils/database.js
+++ utils/database.js
@@ -1,11 +1,7 @@
 import { Pool } from 'pg';
 
 // Creates a global connection pool
-const pool = new Pool({
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+const pool = new Pool();
 
 export const query = (text, params) => {
   return pool.query(text, params);
